5b133703babdd28e33507f6c863ff1fa55f5f097
anseohyun <60808168+anseohyun@users.noreply.github.com>
2022-01-19 22:16:19 +0900

    homepage xml 추가, 관련 페이지들 연결

--- app/src/main/java/softcampus/co/myapplication/HomePage.java
+++ app/src/main/java/softcampus/co/myapplication/HomePage.java
@@ -1,14 +1,67 @@
 package softcampus.co.myapplication;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class HomePage extends AppCompatActivity {
+
+    ImageView main_line;
+    ImageView donation_btn;
+    TextView main_name;
+    TextView main_token_num;
+    TextView main_mil_num;
+    Button using_button;
+    Button donation_button;
+    TextView donation_text;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        ImageView main_line = findViewById(R.id.main_line);
+        ImageView donation_btn = findViewById(R.id.donation_btn);
+        TextView main_name = findViewById(R.id.main_name);
+        TextView main_grade = findViewById(R.id.main_grade);
+        TextView main_token_num = findViewById(R.id.main_token_num);
+        TextView main_mil_num = findViewById(R.id.main_mil_num);
+        Button donation_button = findViewById(R.id.donation_button);
+        TextView donation_text = findViewById(R.id.donation_text);
+        Button using_button = findViewById(R.id.using_button);
+
+        donation_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomePage.this, DonationPage.class);
+                startActivity(intent);
+            }
+        });
+
+        using_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomePage.this, UsageDetails.class);
+                startActivity(intent);
+            }
+        });
+
+        donation_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomePage.this, DonationPage.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/softcampus/co/myapplication/UsageDetails.java
+++ app/src/main/java/softcampus/co/myapplication/UsageDetails.java
@@ -2,20 +2,13 @@ package softcampus.co.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class UsageDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_usage_details);
     }
 }

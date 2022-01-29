ed06926a007ab4e7944424de1fe172212399bf6c
dahyeonlee <tbtzze@gmail.com>
2022-01-29 18:27:57 +0900

    하단바(홈 /마이페이지)

--- app/src/main/java/softcampus/co/myapplication/MainActivity.java
+++ app/src/main/java/softcampus/co/myapplication/MainActivity.java
@@ -1,21 +1,66 @@
 package softcampus.co.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView; //바텀 네비게이션 뷰
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+    private Frag1 frag1_home;
+    private Frag2 frag2_mypage;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);//
+        setContentView(R.layout.activity_main);
 
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        setFrag(0);
+                        break;
+                    case R.id.action_mypage:
+                        setFrag(1);
+                        break;
+                }
+                return true;
+            }
+        });
+        frag1_home = new Frag1();
+        frag2_mypage = new Frag2();
+        setFrag(0); //첫 프래그먼트 화면
+    }
 
+    //프래그먼트 교체가 일어나는 실행문
+    private void setFrag(int n) {
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+        switch (n) {
+            case 0:
+                ft.replace(R.id.main_frame, frag1_home);
+                ft.commit();
+                break;
+            case 1:
+                ft.replace(R.id.main_frame, frag2_mypage);
+                ft.commit();
+                break;
+        }
     }
 }

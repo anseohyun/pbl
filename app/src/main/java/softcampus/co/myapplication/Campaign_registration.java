2e4dcbcb65e3dd4c257651f5cb1c09ea6b406d65
anseohyun <60808168+anseohyun@users.noreply.github.com>
2022-01-30 12:55:46 +0900

    캠페인 설명 부분 수정

--- app/src/main/java/softcampus/co/myapplication/Campaign_registration.java
+++ app/src/main/java/softcampus/co/myapplication/Campaign_registration.java
@@ -3,12 +3,17 @@ package softcampus.co.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Campaign_registration extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_campaign_registration);
+
+
     }
 }

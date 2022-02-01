76f34be3ecdb9e99456c02698a7829add0e1b96d
anseohyun <60808168+anseohyun@users.noreply.github.com>
2022-02-01 21:03:24 +0900

    campaign info xml

---
+++ app/src/main/java/softcampus/co/myapplication/canpaign_infoma.java
@@ -0,0 +1,32 @@
+package softcampus.co.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class canpaign_infoma extends AppCompatActivity {
+
+    ImageView cam_img;
+    TextView cam_name;
+    TextView cam_wri;
+    TextView cam_info;
+    TextView goal_token;
+    Button do_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_canpaign_infoma);
+
+        cam_img = (ImageView) findViewById(R.id.cam_img);
+        cam_name = (TextView) findViewById(R.id.cam_name);
+        cam_wri = (TextView) findViewById(R.id.cam_wri);
+        cam_info = (TextView) findViewById(R.id.cam_info);
+        goal_token = (TextView) findViewById(R.id.goal_token);
+        do_btn = (Button) findViewById(R.id.do_btn);
+
+    }
+}

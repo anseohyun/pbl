128004920115db51f2beb94014139560cc2f81f7
anseohyun <60808168+anseohyun@users.noreply.github.com>
2022-02-04 20:54:11 +0900

    로그인 화면으로 이동

--- app/src/main/java/softcampus/co/myapplication/SplashActivity.java
+++ app/src/main/java/softcampus/co/myapplication/SplashActivity.java
@@ -18,7 +18,7 @@ public class SplashActivity extends AppCompatActivity {
         Handler handler=new Handler();
         handler.postDelayed(new Runnable(){
             public void run(){
-                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
                 startActivity(intent);
                 finish();
             }

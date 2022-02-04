6e23e3168d9dd2ae73904ccb8dcc561b1f1f7fd2
lovelove0618 <lovelove0618@naver.com>
2022-02-04 16:47:39 +0900

    첫화면 스플래시, 로그인 회원가입 기능 추가

---
+++ app/src/main/java/softcampus/co/myapplication/UserAccount.java
@@ -0,0 +1,25 @@
+package softcampus.co.myapplication;
+
+public class UserAccount {
+
+    private String idToken; //Firebase Uid (고유 토큰정보)
+    private String emailId;
+    private String password;
+
+    public UserAccount() { }
+
+    public String getIdToken() { return idToken; }
+
+    public void setIdToken(String idToken) { this.idToken = idToken; }
+
+    public String getEmailId(){ return emailId; }
+
+    public void setEmailId(String emailId) { this.emailId = emailId; }
+
+    public String getPassword() { return password; }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+}

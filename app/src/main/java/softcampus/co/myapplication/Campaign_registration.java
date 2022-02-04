4a80e0c0301d3240452339cc62de14ea0d5774c7
anseohyun <60808168+anseohyun@users.noreply.github.com>
2022-02-04 11:11:04 +0900

    캠페인 갤러리에서 이미지 넣기

--- app/src/main/java/softcampus/co/myapplication/Campaign_registration.java
+++ app/src/main/java/softcampus/co/myapplication/Campaign_registration.java
@@ -1,8 +1,12 @@
 package softcampus.co.myapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,8 +24,10 @@ public class Campaign_registration extends AppCompatActivity {
     EditText regi_token_input;
     TextView regi_exp;
     EditText regi_exp_input;
-    Button regi_img;
     Button regi_btn;
+
+    private int GET_GALLERY_IMAGE = 200;
+    private ImageView imageview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +42,7 @@ public class Campaign_registration extends AppCompatActivity {
         regi_token_input = (EditText) findViewById(R.id.regi_token_input);
         regi_exp = (TextView) findViewById(R.id.regi_exp);
         regi_exp_input = (EditText) findViewById(R.id.regi_exp_input);
-        regi_img = (Button) findViewById(R.id.regi_img);
+        imageview = (ImageView) findViewById(R.id.imageview);
         regi_btn = (Button) findViewById(R.id.regi_btn);
 
         regi_btn.setOnClickListener(new View.OnClickListener() {
@@ -46,6 +52,27 @@ public class Campaign_registration extends AppCompatActivity {
             }
         });
 
+        imageview.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent. setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(intent, GET_GALLERY_IMAGE);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri selectedImageUri = data.getData();
+            imageview.setImageURI(selectedImageUri);
+
+        }
 
     }
 }
